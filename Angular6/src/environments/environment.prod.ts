47f71578f69561ed24112da674e0a93fb63529df
doswaldo7423 <daniel.sierra@easytrack.mx>
2023-08-24 16:50:57 -0600

    Mvp1

--- Angular6/src/environments/environment.prod.ts
+++ Angular6/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://18.237.220.63:3000/api'
+  //apiBaseUrl: 'http://18.237.220.63:3000/api'
+  apiBaseUrl: 'http://lb-internal-280269980.us-west-2.elb.amazonaws.com:3000/api'
 };
