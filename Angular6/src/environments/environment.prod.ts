3a8e8a721675f72d43975988ad1b78411c3e8077
doswaldo7423 <daniel.sierra@easytrack.mx>
2023-08-26 10:22:23 -0600

    Mvp1

--- Angular6/src/environments/environment.prod.ts
+++ Angular6/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   //apiBaseUrl: 'http://18.237.220.63:3000/api'
   //apiBaseUrl: 'https://lb-internal-649076184.us-west-2.elb.amazonaws.com/api'
-  apiBaseUrl: 'http://lb-back-1769099188.us-west-2.elb.amazonaws.com/api'
+  apiBaseUrl: 'https://api.arsmarty.com/api'
 };
